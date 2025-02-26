dc55db4abf9a0fbe8509c1935e38ada12e775c99
mimilibri <mimamihajlovic@hotmail.com>
2025-02-26 22:40:23 +0100

    Repository pattern practice Get all orders endpint w/o pagination

---
+++ Repository/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPractice.Data;
+using WebApiPractice.DTO;
+using WebApiPractice.Extensions;
+using WebApiPractice.Interfaces;
+using WebApiPractice.Models;
+
+namespace WebApiPractice.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _context ;
+        public OrderRepository(ApplicationDbContext context)
+        {
+            _context = context ;
+        }
+        public ICollection<Order> GetAllOrders()
+        {
+            return _context.Order.OrderBy(x=>x.TotalPrice).ToList();
+        }
+
+        public ICollection<OrderDto> GetOrdersWithLines() 
+        {
+            var orders = _context.Order
+                .Include(p=>p.Products)
+                .Select(p=>p.ToDto())
+                .ToList();
+
+            return orders;
+        
+        }
+    }
+}
